33d31134c034454cb9670f5e18450bb3bddda388
Nandakumar <a_nanadakumar@ats-hr420-dm13.fc.ca>
2019-10-29 13:34:15 -0400

    Modified game

--- game.py
+++ game.py
@@ -8,23 +8,41 @@ computer = choices[randint(0,2)]
 player = False
 
 while player == False:
-
-	player = input("Choose between rock , paper and scissors\n")
-
+    
+	print("--------------------------------------------------")
+	player = input("Choose between rock , paper and scissors\n\n")
+	print("------------------------------------------------")
 
 	print("Computer chose",computer, "\n")               
 	print("Player chose",player, "\n")
 
-	if player =="quit":
+	if player.lower() =="quit":
 		exit()
 	elif computer == player:
 		print ("Tie ! no one wins , play again")
+	elif player.lower() == "rock":
+	    if computer == "paper":
+	    	print("You lose !!",computer,"Covers",player,"\n")
+	    else:
+	     	print("You win!!!!",player,"smashes",computer,"\n")
+	elif player.lower() == "paper":
+	    if computer == "scissors":
+	    	print("You lose !!",computer,"cuts",player,"\n")
+	    else:
+	     	print("You win!!!!",player,"covers",computer,"\n")
+	elif player.lower() == "scissors":
+	    if computer == "rock":
+	    	print("You lose !!",computer,"smashes",player,"\n")
+	    else:
+	     	print("You win!!!!",player,"cuts",computer,"\n")
+
+	else:
+		print("Thats not a valid choice,try again")     	    	    	   	
 
 	player = False
-	computer = choices[randint(0,2)]			
+   	computer = choices[randint(0,2)]			
 
-
-
+    			
 
 
 
